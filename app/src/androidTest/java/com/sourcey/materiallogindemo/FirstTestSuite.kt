a25c13dc4693f0f7fc71c46f2eaac1bdeb2210d7
KANITTHA HARNRATTANAPRASERT <kanittha.harnrattanaprasert@scb.co.th>
2020-11-24 14:30:33 +0700

    update dependencies

--- app/src/androidTest/java/com/sourcey/materiallogindemo/FirstTestSuite.kt
+++ app/src/androidTest/java/com/sourcey/materiallogindemo/FirstTestSuite.kt
@@ -4,13 +4,30 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+//class FirstTestSuite {
+//    @get:Rule
+//    val activityRule = ActivityTestRule(MainActivity::class.java)
+//
+//    @Test
+//    fun test01() {
+//        onView(withText("LOGIN")).check(matches(isDisplayed()))
+//    }
+//}
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
 class FirstTestSuite {
+
     @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Test
     fun test01() {
